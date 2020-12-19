45232af4022b31df819d5e6f479e2ccc6c4bb002
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-12-19 21:32:03 +0100

    model (almost) Paint entity

--- src/entity/Paint.ts
+++ src/entity/Paint.ts
@@ -1,1 +1,20 @@
-export class Paint {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum PaintType {
+    Base = 'base',
+    Layer = 'layer',
+    Shade = 'shade',
+    Technical = 'technical'
+}
+
+@Entity()
+export class Paint {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({type: 'enum', enum: PaintType, default: PaintType.Base})
+    type!: PaintType;
+
+    // TODO: add relation with PaintScheme
+}
