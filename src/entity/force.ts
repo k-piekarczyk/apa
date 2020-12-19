71a5832b5bd6261780dd119ea62c8c720218fa0c
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-12-19 21:05:30 +0100

    model Force entity

--- src/entity/force.ts
+++ src/entity/force.ts
@@ -1,8 +1,17 @@
-import {Entity, PrimaryColumn} from 'typeorm';
+import { type } from 'os';
+import {Entity, OneToMany, PrimaryColumn} from 'typeorm';
+import { Army } from './Army';
+import { Unit } from './Unit';
 
 @Entity()
 export class Force {
     
     @PrimaryColumn({length: 32})
     name!: string;
+
+    @OneToMany(type => Army, army => army.force)
+    armies!: Army[];
+
+    @OneToMany(type => Unit, unit => unit.force)
+    units!: Unit[];
 }
