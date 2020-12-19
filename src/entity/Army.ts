47d31a96e024c826ae8cda4d53f62772f0f012e4
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-12-19 21:17:27 +0100

    add entity scaffolding

---
+++ src/entity/Army.ts
@@ -0,0 +1,27 @@
+import { type } from 'os';
+import {Entity, PrimaryColumn, Column, ManyToOne, OneToMany} from 'typeorm';
+import { Force } from './Force';
+import { UnitVariant } from './UnitVariant';
+import { User } from './user';
+
+@Entity()
+export class Army {
+
+    @PrimaryColumn({length: 256})
+    name!: string;
+
+    @ManyToOne(type => User, user => user.armies)
+    user!: User;
+
+    @Column({length: 254, unique: true})
+    email!: string
+
+    @Column({length: 60})
+    passwordHash!: string
+
+    @ManyToOne(type => Force, force => force.armies)
+    force!: Force;
+
+    @OneToMany(type => UnitVariant, unitVariant => unitVariant.army)
+    units!: UnitVariant[];
+}
