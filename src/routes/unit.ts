38b7f4f025dfda224733a60a434bf8e992348d58
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-12-22 22:51:22 +0100

    add all unit views

--- src/routes/unit.ts
+++ src/routes/unit.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { getRepository } from "typeorm";
 import { RelationLoader } from "typeorm/query-builder/RelationLoader";
 import { Force } from "../entity/Force";
+import { PaintScheme } from "../entity/PaintScheme";
 import { Unit } from "../entity/Unit";
 import { IRequest } from "../interfaces/request";
 import { verifiedUser } from "../middleware/auth";
@@ -18,24 +19,26 @@ export class UnitRouter extends CommonRouter {
             .get("/", this.unitList.bind(this))
             .get("/add", this.addUnitView.bind(this))
             .post("/add", this.addUnit.bind(this))
+            .get("/:unitID", this.unitDetail.bind(this))
+            .get("/:unitID/scheme/:schemeID/add", this.addSchemeToUnit.bind(this))
     }
 
     async unitList(req: IRequest, res: Response) {
         const units = await getRepository(Unit).find();
-        return res.render("unit/listUnit", {units});
+        return res.render("unit/listUnit", { units });
     }
 
     async addUnitView(req: IRequest, res: Response) {
         const forces = await getRepository(Force).find()
-        return res.render("unit/addUnit", {forces});
+        return res.render("unit/addUnit", { forces });
     }
 
     async addUnit(req: IRequest, res: Response) {
-        const {name, forceName, type, minPerUnit, maxPerUnit, pointsPerModel} = req.body;
+        const { name, forceName, type, minPerUnit, maxPerUnit, pointsPerModel } = req.body;
 
         let newUnit = new Unit();
         newUnit.name = name;
-        newUnit.force = await getRepository(Force).findOne({name: forceName});
+        newUnit.force = await getRepository(Force).findOne({ name: forceName });
         newUnit.type = type;
         newUnit.minModelsPerUnit = minPerUnit;
         newUnit.maxModelsPerUnit = maxPerUnit;
@@ -52,5 +55,31 @@ export class UnitRouter extends CommonRouter {
         }
 
         return res.redirect("/unit");
+    }
+
+    async unitDetail(req: IRequest, res: Response) {
+        const unitID = Number.parseInt(req.params.unitID);
+
+        const unit = await getRepository(Unit).findOne({id: unitID}, {relations: ["paintSchemes"]});
+        const schemes = await getRepository(PaintScheme).find();
+
+        return res.render("unit/unitDetail", {
+            unit,
+            schemes
+        })
+    }
+
+    async addSchemeToUnit(req: IRequest, res: Response) {
+        const unitID = Number.parseInt(req.params.unitID);
+        const schemeID = Number.parseInt(req.params.schemeID);
+
+        const unit = await getRepository(Unit).findOne({id: unitID}, {relations: ["paintSchemes"]});
+        const scheme = await getRepository(PaintScheme).findOne({id: schemeID});
+
+        unit.paintSchemes.push(scheme);
+
+        await getRepository(Unit).save(unit);
+
+        return res.redirect("/unit/" + unitID);
     }
 }
